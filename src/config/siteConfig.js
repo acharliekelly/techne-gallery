1a0e1c41c0dade7ce0ae9b7b11e6dfcf353c1188
Charlie Kelly <charliekelly31415@gmail.com>
2025-07-26 18:00:13 -0400

    project config

---
+++ src/config/siteConfig.js
@@ -0,0 +1,10 @@
+// File: src/config/siteConfig.js
+export default {
+  siteTitle: 'Techne Gallery',
+  domain: 'techne-gallery.com', // change later
+  author: 'Charlie Kelly',
+  social: {
+    github: 'https://github.com/acharliekelly',
+    linkedin: 'https://linkedin.com/in/acharliekelly'
+  },
+};
